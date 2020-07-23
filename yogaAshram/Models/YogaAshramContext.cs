b597fb05bae7af128de8127fa193553543252626
Iskander Dosgali <idosgali@gmail.com>
2020-07-23 12:13:42 +0600

    #3 Изменение добавил YogaAshramContext.cs поле Db Employees

--- yogaAshram/Models/YogaAshramContext.cs
+++ yogaAshram/Models/YogaAshramContext.cs
@@ -5,6 +5,7 @@ namespace yogaAshram.Models
 {
     public class YogaAshramContext : IdentityDbContext<Employee>
     {
+        public DbSet<Employee> Employees { get; set; }
         
         public YogaAshramContext(DbContextOptions options) : base(options)
         {
