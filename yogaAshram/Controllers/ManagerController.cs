e0adc65c9c2377106156f5c6021a1b9a3c68b95b
Iskander Dosgali <idosgali@gmail.com>
2020-07-26 13:42:23 +0600

    #12 Создал ManagerController.cs

---
+++ yogaAshram/Controllers/ManagerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace yogaAshram.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
