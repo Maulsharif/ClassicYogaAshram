dfeca456dc70c6e2364816e9f945e6a0ae695cb7
Iskander Dosgali <idosgali@gmail.com>
2020-07-27 15:09:40 +0600

    #18 Добавил поля для редактирование в EditModelView.cs

--- yogaAshram/Models/ModelViews/EditModelView.cs
+++ yogaAshram/Models/ModelViews/EditModelView.cs
@@ -2,6 +2,10 @@
 {
     public class EditModelView
     {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
         
+        public string NameSurname { get; set; }
     }
 }
