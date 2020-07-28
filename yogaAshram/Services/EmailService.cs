edf5f3c88a3701bac60072cdb429805a94cc35a1
Iskander Dosgali <idosgali@gmail.com>
2020-07-28 15:13:08 +0600

    #20 Добавил в service EmailService.cs логику по Smtp отправкой  yandex.ru

--- yogaAshram/Services/EmailService.cs
+++ yogaAshram/Services/EmailService.cs
@@ -1,7 +1,34 @@
-﻿namespace yogaAshram.Services
+﻿using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace yogaAshram.Services
 {
     public class EmailService
     {
+        public async Task SendMessageAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
         
+            emailMessage.From.Add(new MailboxAddress(
+                "Администратор сайта",
+                "doskalii@yandex.ru"
+            ));
+        
+            emailMessage.To.Add(new MailboxAddress("", email));
+        
+            emailMessage.Subject = subject;
+        
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+            
+            using var client = new SmtpClient();
+            await client.ConnectAsync("smtp.yandex.ru", 25, false);
+            await client.AuthenticateAsync("doskalii@yandex.ru", "mamapapa+18");
+            await client.SendAsync(emailMessage);
+            await client.DisconnectAsync(true);
+        }
     }
 }
