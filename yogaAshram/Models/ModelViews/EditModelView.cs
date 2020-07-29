2748ebef95204c83576b48e078777cd660033746
Iskander Dosgali <idosgali@gmail.com>
2020-07-29 17:10:18 +0600

    #18 Добавил валидацию EditModelView.cs для редактирование пользователя

--- yogaAshram/Models/ModelViews/EditModelView.cs
+++ yogaAshram/Models/ModelViews/EditModelView.cs
@@ -1,11 +1,14 @@
-﻿﻿namespace yogaAshram.Models.ModelViews
+﻿﻿using Microsoft.AspNetCore.Mvc;
+
+ namespace yogaAshram.Models.ModelViews
 {
     public class EditModelView
     {
         public string Id { get; set; }
+        [Remote(action: "CheckUserNameCreate", controller: "Validation", ErrorMessage = "Такой логин уже существует")]
         public string UserName { get; set; }
+        [Remote(action: "CheckEmailCreate", controller: "Validation", ErrorMessage = "Такая почта уже существует")]
         public string Email { get; set; }
-        
         public string NameSurname { get; set; }
     }
 }
