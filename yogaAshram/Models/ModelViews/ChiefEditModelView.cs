bb435cd661afd5129b322754bfc21bf5b629694b
Raimbek Takshibaev <rashi24e@gmail.com>
2020-07-29 16:03:20 +0600

    #17 написал модели представления для редактирования пользователя и добавил ее в еще одно представление деталей

---
+++ yogaAshram/Models/ModelViews/ChiefEditModelView.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yogaAshram.Models.ModelViews
+{
+    public class ChiefEditModelView
+    {
+        [Required(ErrorMessage = "Введите логин")]
+        [Remote(action: "CheckEditUserName", controller: "Vaidation", ErrorMessage = "Аккаунт с таким логином уже зарегистрирован")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Введите почту")]
+        [Remote(action: "CheckEditEmail", controller: "Vaidation", ErrorMessage = "Аккаунт с такой почтой уже зарегистрирован")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Введите имя и фамилию")]
+        public string NameSurname { get; set; }
+    }
+}
