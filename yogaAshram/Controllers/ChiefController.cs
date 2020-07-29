123843ebc8e9aa90bffee6078ef6ada220cc0dc4
Raimbek Takshibaev <rashi24e@gmail.com>
2020-07-29 16:02:37 +0600

    #17 написал метод редактирования и метод получения модального окна редактирования профиля

--- yogaAshram/Controllers/ChiefController.cs
+++ yogaAshram/Controllers/ChiefController.cs
@@ -40,33 +40,24 @@ namespace yogaAshram.Controllers
             }
             return null;
         }
-        [HttpPost]
-        public async Task<bool> ResetPasswordAjax()
+        private async Task SetViewBagRoles()
         {
-            Employee employee = await _userManager.GetUserAsync(User);
-            string psw = PasswordGenerator.Generate();
-            string code = await _userManager.GeneratePasswordResetTokenAsync(employee);
-            var result = await _userManager.ResetPasswordAsync(employee, code, psw);
- //           await EmailService.SendPassword(employee.Email, psw, Url.Action());
-            employee.OnTimePassword = true;
-            _db.Entry(employee).State = EntityState.Modified;
-            await _db.SaveChangesAsync();
-            return result.Succeeded;
+            Dictionary<string, string> rolesDic = new Dictionary<string, string>();
+            var roles = await _db.Roles.Where(p => p.Name != "chief").ToArrayAsync();
+            foreach (var item in roles)
+                rolesDic.Add(item.Name, GetRuRoleName(item.Name));
+            ViewBag.Roles = rolesDic;
         }
         public async Task<IActionResult> Index()
         {
             Employee empl = await _userManager.GetUserAsync(User);
-            Dictionary<string, string> rolesDic = new Dictionary<string, string>();
-            var roles = await _db.Roles.ToArrayAsync();
-            foreach (var item in roles)
-                rolesDic.Add(item.Name, GetRuRoleName(item.Name));
-            ViewBag.Roles = rolesDic;
+            await SetViewBagRoles();
             return View(new ChiefIndexModelView() { Employee = empl });
         }
         [HttpPost]    
-        public async Task<IActionResult> CreateEmployee(string nameSurname, string userName, string email)
+        public async Task<IActionResult> CreateEmployee(string nameSurname, string userName, string email, string role)
         {
-            if (String.IsNullOrEmpty(nameSurname))
+            if (String.IsNullOrEmpty(nameSurname) || !_db.Roles.Any(p => p.Name == role) || role == "chief")
                 return BadRequest();
             Employee employee = new Employee()
             {
@@ -78,6 +69,7 @@ namespace yogaAshram.Controllers
             var result = await _userManager.CreateAsync(employee, newPsw);
             if (result.Succeeded)
             {
+                await _userManager.AddToRoleAsync(employee, role);
                 await EmailService.SendPassword(email, newPsw, Url.Action("Login", "Account", null, Request.Scheme));
                 return Json("true");
             }
@@ -85,6 +77,31 @@ namespace yogaAshram.Controllers
             foreach (var error in result.Errors)
                 errors += error.Description;
             return Json(errors);
+        }
+        public async Task<IActionResult> GetEditModalAjax()
+        {
+            Employee employee = await _userManager.GetUserAsync(User);
+            return PartialView("PartialViews/ChiefAccountEditPartial", new ChiefEditModelView { 
+                Email = employee.Email,
+                NameSurname = employee.NameSurname,
+                UserName = employee.UserName
+            });
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(ChiefEditModelView model)
+        {
+            Employee employee = await _userManager.GetUserAsync(User);
+            if (ModelState.IsValid)
+            {             
+                employee.UserName = model.UserName;
+                employee.Email = model.Email;
+                employee.NameSurname = model.NameSurname;
+                _db.Entry(employee).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            await SetViewBagRoles();
+            return View("Chief/Index", new ChiefIndexModelView() { EditModel = model, IsEditInvalid = true, Employee = employee });
         }
         [HttpPost]
         public async Task<IActionResult> ChangePassword(ChangePasswordModelView model)
@@ -104,11 +121,7 @@ namespace yogaAshram.Controllers
                     return RedirectToAction("Index");
                 }               
             }
-            Dictionary<string, string> rolesDic = new Dictionary<string, string>();
-            var roles = await _db.Roles.ToArrayAsync();
-            foreach (var item in roles)
-                rolesDic.Add(item.Name, GetRuRoleName(item.Name));
-            ViewBag.Roles = rolesDic;
+            await SetViewBagRoles();
             return View("../Chief/Index", new ChiefIndexModelView() { Employee = employee, Model = model, IsModalInvalid = true }) ;
         }
     }
