ac67b78069d3e528203b40cf9d4c51401837b9d3
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-07-30 15:46:01 +0600

    #24 добавил новые миграции

---
+++ yogaAshram/Migrations/20200730094155_newModelManEmpl.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace yogaAshram.Migrations
+{
+    public partial class newModelManEmpl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ManagerEmployees",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ManagerId = table.Column<long>(nullable: false),
+                    EmployeeId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManagerEmployees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ManagerEmployees_AspNetUsers_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ManagerEmployees_AspNetUsers_ManagerId",
+                        column: x => x.ManagerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ManagerEmployees_EmployeeId",
+                table: "ManagerEmployees",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ManagerEmployees_ManagerId",
+                table: "ManagerEmployees",
+                column: "ManagerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ManagerEmployees");
+        }
+    }
+}
