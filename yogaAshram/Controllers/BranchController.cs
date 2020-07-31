a6c544a44bd2f4919b658fe1fa6eed16b58a9614
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-07-31 13:22:20 +0600

    #34 добавил действие в контроллер для редактирования центра/филиала

--- yogaAshram/Controllers/BranchController.cs
+++ yogaAshram/Controllers/BranchController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -22,16 +23,49 @@ namespace yogaAshram.Controllers
         }
         
         [HttpPost]
-        public async Task<IActionResult> CreateBranch(string name, string info, string address)
+        public async Task<IActionResult> CreateBranch(string name, string info, string address, long? marketerId, long? sellerId)
         {
             Branch branch = new Branch()
             {
                 Name = name,
                 Info = info,
-                Address = address
+                Address = address,
+                MarketerId = marketerId,
+                SellerId = sellerId
             };
             _db.Entry(branch).State = EntityState.Added;
             await _db.SaveChangesAsync();
+            return RedirectToAction("Index", "Chief");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(long id, 
+            string name, 
+            string address,
+            string info,
+            long? sellerId,
+            long? marketerId)
+        {
+            Branch branch = _db.Branches.FirstOrDefault(p => p.Id == id);
+            if (branch != null)
+            {
+                if (name != null)
+                    branch.Name = name;
+                
+                else if(address != null)
+                    branch.Address = address;
+                
+                else if(info != null)
+                    branch.Info = info;
+                
+                else if(sellerId != null)
+                    branch.SellerId = sellerId;
+                
+                else if(marketerId != null)
+                    branch.MarketerId = marketerId;
+
+                _db.Entry(branch).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
+            }
             return RedirectToAction("Index", "Chief");
         }
     }
