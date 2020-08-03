885689ac067b72a58789a3029c49286e05a0158c
Iskander Dosgali <idosgali@gmail.com>
2020-08-03 16:33:00 +0600

    #42 Востоновил CreateGroupModelView.cs модель

---
+++ yogaAshram/Models/ModelViews/CreateGroupModelView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace yogaAshram.Models.ModelViews
+{
+    public class CreateGroupModelView
+    {
+        public long Id { get; set; }
+        public string  Name { get; set; }
+        public long BranchId { get; set; }
+        public virtual Branch Branch { get; set; }
+        public string  CoachName { get; set; }
+        public int MaxCapacity { get; set; } = 16;
+        public int MinCapacity { get; set; } = 10;
+        public long  CreatorId { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
