c30b01d0d7ef06c9381178dc7fc39deda9afc20c
Iskander Dosgali <idosgali@gmail.com>
2020-08-03 22:50:50 +0600

    #42 В CreateGroupModelView.cs к поля добавил валидаций

--- yogaAshram/Models/ModelViews/CreateGroupModelView.cs
+++ yogaAshram/Models/ModelViews/CreateGroupModelView.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
 
 namespace yogaAshram.Models.ModelViews
 {
     public class CreateGroupModelView
     {
         public long Id { get; set; }
+        [Required(ErrorMessage = "Введите наименование группы")]
         public string  Name { get; set; }
+        [Remote(action: "CheckCreate", controller: "Validation", ErrorMessage = "Такого филиала нет !!!")]
         public long BranchId { get; set; }
         public virtual Branch Branch { get; set; }
+        [Required(ErrorMessage = "Введите имя тенера")]
         public string  CoachName { get; set; }
         
         public int MaxCapacity { get; set; } = 16;
