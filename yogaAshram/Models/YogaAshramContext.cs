5c42cf551e6b023811666a9a62bc727467266e92
Iskander Dosgali <idosgali@gmail.com>
2020-08-05 15:56:32 +0600

    #50 В YogaAshramContext.cs добавил дб Clients

--- yogaAshram/Models/YogaAshramContext.cs
+++ yogaAshram/Models/YogaAshramContext.cs
@@ -9,8 +9,13 @@ namespace yogaAshram.Models
         public DbSet<Employee> Employees { get; set; }
         public DbSet<ManagerEmployee> ManagerEmployees { get; set; }
         public DbSet<Branch> Branches { get; set; }
+        
         public DbSet<Group> Groups { get; set; }
+        
         public DbSet<Schedule> Schedules { get; set; }
+        
+        public DbSet<Client> Clients { get; set; }
+
         public YogaAshramContext(DbContextOptions options) : base(options) 
         {
         }
