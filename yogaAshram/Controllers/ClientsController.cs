d08e46afe39c7faec871aaca9a3748b3ceced5da
Iskander Dosgali <idosgali@gmail.com>
2020-08-05 15:55:19 +0600

    #50 Добавил Enum ClientType

--- yogaAshram/Controllers/ClientsController.cs
+++ yogaAshram/Controllers/ClientsController.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using yogaAshram.Models;
 
 namespace yogaAshram.Controllers
 {
+    public enum ClientType
+    {
+        Probe,
+        AreEngaged,
+        NotEngaged
+    }
+    
     public class ClientsController : Controller
     {
-        // GET
+        private readonly UserManager<Employee> _userManager;
+        private readonly SignInManager<Employee> _signInManager;
+        private readonly YogaAshramContext _db;
+
+        public ClientsController(UserManager<Employee> userManager, SignInManager<Employee> signInManager, YogaAshramContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+        
         public IActionResult Index()
         {
             return View();
