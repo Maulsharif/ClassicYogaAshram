31c57b80211f4be288762f08adc9bb1a1bd62b0f
Iskander Dosgali <idosgali@gmail.com>
2020-08-05 15:39:55 +0600

    #50 Создал добавил новый контроллер ClientsController.cs

---
+++ yogaAshram/Controllers/ClientsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace yogaAshram.Controllers
+{
+    public class ClientsController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
