01ad142ba0b17dd6db5d8e1457d8714013633075
Iskander Dosgali <idosgali@gmail.com>
2020-08-05 15:38:59 +0600

    #50 Создал добавил новый модель Client.cs

---
+++ yogaAshram/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿namespace yogaAshram.Models
+{
+    public class Client
+    {
+        public long Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        
+        public string ClientType { get; set; }
+
+        public long GroupId { get; set; }
+        
+        public virtual Group Group { get; set; }
+    }
+}
