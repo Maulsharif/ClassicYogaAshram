8f885b1ae990baba768d678b32ebd7c20a7a1401
Iskander Dosgali <idosgali@gmail.com>
2020-08-05 16:00:04 +0600

    #50 В ClientsCreateModelView.cs добавил поля нужные

--- yogaAshram/Models/ModelViews/ClientsCreateModelView.cs
+++ yogaAshram/Models/ModelViews/ClientsCreateModelView.cs
@@ -1,7 +1,17 @@
-﻿namespace yogaAshram.Models.ModelViews
+﻿using yogaAshram.Controllers;
+
+namespace yogaAshram.Models.ModelViews
 {
     public class ClientsCreateModelView
     {
+        public string NameSurname { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public ClientType ClientType { get; set; } = ClientType.Probe;
+
+        public long GroupId { get; set; }
         
+        public virtual Group Group { get; set; }
     }
 }
