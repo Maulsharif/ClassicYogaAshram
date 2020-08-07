07be508d941d8e6674b74f3c9b3dd3ac53863d5d
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-08-07 22:26:04 +0600

    #43 изменил поля модели Schedule.cs

--- yogaAshram/Models/Schedule.cs
+++ yogaAshram/Models/Schedule.cs
@@ -2,58 +2,18 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace yogaAshram.Models
-{
-    public enum DayOfWeek
-    {
-        monday=1, tuesday, wednesday,thursday, friday, saturday,sunday
-    }
-    public class Schedule
-    {
-        public long Id { get; set; }
-        public DayOfWeek DayOfWeek { get; set; } = DayOfWeek.monday;
-        [Required(ErrorMessage = "Введите значение")]
-        
-        public int FromHours { get; set; }
-        [Required(ErrorMessage = "Введите значение")]
-        public int FromMinutes { get; set; }
-        public int ToHours { get; set; }
-        [Required(ErrorMessage = "Введите значение")]
-        public int ToMinutes { get; set; }
-        public long GroupId { get; set; }
-        public virtual Group Group{ get; set; }
-        public virtual List<Group> Groups { get; set; }
 
-        public string GetEnumValue()
+    namespace yogaAshram.Models
+    {
+        public class Schedule
         {
-            string day = "";
-            switch (DayOfWeek) 
-            {
-                case DayOfWeek.monday:
-                    day = "понедельник";
-                    break;
-                case DayOfWeek.tuesday:
-                    day = "вторник";
-                    break;
-                case DayOfWeek.wednesday:
-                    day = "среду";
-                    break;
-                case DayOfWeek.thursday:
-                    day = "четверг";
-                    break;
-                case DayOfWeek.friday:
-                    day = "пятницу";
-                    break;
-                case DayOfWeek.saturday:
-                    day = "субботу";
-                    break;
-                case DayOfWeek.sunday:
-                    day = "воскресенье";
-                    break;
-            }
-
-            return day;
+            public long Id { get; set; }
+            public List<DateTime> FromDate { get; set; }
+            public List<DateTime> ToDate { get; set; }
+            public long GroupId { get; set; }
+            public virtual Group Group{ get; set; }
+            public virtual List<Group> Groups { get; set; }
         }
     }
+
    
-}
