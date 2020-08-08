7955debc0d304b439fc8f1080e59c2059d19df36
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-08-08 16:24:05 +0600

    #43 Изменил поля Schedule.cs повторно

--- yogaAshram/Models/Schedule.cs
+++ yogaAshram/Models/Schedule.cs
@@ -8,8 +8,11 @@ using System.ComponentModel.DataAnnotations;
         public class Schedule
         {
             public long Id { get; set; }
-            public List<DateTime> FromDate { get; set; }
-            public List<DateTime> ToDate { get; set; }
+            public long BranchId { get; set; }
+            public virtual Branch Branch { get; set; }
+            public List<string> DayOfWeeksString { get; set; }
+            public TimeSpan StartTime { get; set; }
+            public TimeSpan FinishTime { get; set; }
             public long GroupId { get; set; }
             public virtual Group Group{ get; set; }
             public virtual List<Group> Groups { get; set; }
