0038736dd4367557f439bd0eee68aebbaa952f8b
Raimbek Takshibaev <rashi24e@gmail.com>
2020-08-12 16:43:39 +0600

    #63 убрал ненужные модели представления

--- yogaAshram/Models/ModelViews/ChiefIndexModelView.cs
+++ yogaAshram/Models/ModelViews/ChiefIndexModelView.cs
@@ -7,11 +7,7 @@ namespace yogaAshram.Models.ModelViews
 {
     public class ChiefIndexModelView
     {
-        public bool IsModalInvalid { get; set; } = false;
         public Employee Employee { get; set; }
-        public bool IsEditInvalid { get; set; } = false;
-        public ChiefEditModelView EditModel { get; set; } = new ChiefEditModelView();
-        public ChangePasswordModelView Model { get; set; } = new ChangePasswordModelView();
         public CheckPasswordModelView CheckPassword { get; set; } = new CheckPasswordModelView();
     }
 }
