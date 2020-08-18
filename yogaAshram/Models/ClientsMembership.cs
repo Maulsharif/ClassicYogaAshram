70ffa11b2a2ea3daa4842cb8f29720482cfc701f
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-08-18 21:25:53 +0600

    #80 добавил новую сущность ClientsMembership.cs

---
+++ yogaAshram/Models/ClientsMembership.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace yogaAshram.Models
+{
+    public class ClientsMembership
+    {
+        public long Id { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+        
+        public long ClientId { get; set; }
+        public Client Client { get; set; }
+        
+        public long MembershipId { get; set; }
+        public Membership Membership { get; set; }
+    }
+}
