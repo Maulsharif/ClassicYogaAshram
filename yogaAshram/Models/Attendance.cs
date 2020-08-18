c2637cd68da646d88c1589c15b459d1b622e7934
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-08-18 21:30:50 +0600

    #80 изменил сущность Attendance.cs

--- yogaAshram/Models/Attendance.cs
+++ yogaAshram/Models/Attendance.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace yogaAshram.Models
@@ -17,9 +18,13 @@ namespace yogaAshram.Models
         public virtual Client  Client { get; set; }
         public long  GroupId  { get; set; }
         public virtual Group  Group { get; set; }
-        public DateTime Date { get; set; }
+        public int AttendanceDays { get; set; }
         public AttendanceState AttendanceState { get; set; } = Models.AttendanceState.notcheked;
         public bool IsChecked { get; set; } = false;
+        public List<DateTime> DatesOfAttendance { get; set; }
+        public List<DateTime> DatesOfAbsence { get; set; }
+        
+
         
     }
 }
