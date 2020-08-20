d4ec9892ffb1acac5b8b33aa8f835f237e4bba64
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-08-20 18:40:50 +0600

    #80 добавил сервис для клиентов для работы с датами

---
+++ yogaAshram/Services/ClientServices.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using yogaAshram.Models;
+
+namespace yogaAshram.Services
+{
+    public class ClientServices
+    {
+        private readonly YogaAshramContext _db;
+
+        public ClientServices(YogaAshramContext db)
+        {
+            _db = db;
+        }
+
+        public List<DateTime> DatesForAttendance(DateTime firstTime, long groupId, int attendanceDays)
+        {
+            List<CalendarEvent> calendarEvents = _db.CalendarEvents.Where(c => c.GroupId == groupId).ToList();
+            DayOfWeek[] dayOfWeeks = new DayOfWeek[calendarEvents.Count];
+            for (int i = 0; i < calendarEvents.Count; i++)
+            {
+                dayOfWeeks[i] = calendarEvents[i].DayOfWeek;
+            }
+            DateTime fortyDays = firstTime.AddDays(40);
+            
+            List<DateTime> dates = Enumerable.Range(0, 1 + fortyDays.Subtract(firstTime).Days)
+                .Select(offset => firstTime.AddDays(offset))
+                .Where(d => dayOfWeeks.Contains(d.DayOfWeek))
+                .ToList();
+            
+            DateTime [] dateTimes = new DateTime[attendanceDays];
+            for (int i = 0; i < attendanceDays; i++)
+            {
+                dateTimes[i] = dates[i];
+            }
+            
+            return dateTimes.ToList();
+        } 
+        public List<DateTime> TwoTimesTrial(long? groupId, DateTime firstTime)
+        {
+            List<CalendarEvent> calendarEvents = _db.CalendarEvents.Where(c => c.GroupId == groupId).ToList();
+            DayOfWeek[] dayOfWeeks = new DayOfWeek[calendarEvents.Count];
+            for (int i = 0; i < calendarEvents.Count; i++)
+            {
+                dayOfWeeks[i] = calendarEvents[i].DayOfWeek;
+            }
+            DateTime tenDays = firstTime.AddDays(10);
+            
+            List<DateTime> dates = Enumerable.Range(0, 1 + tenDays.Subtract(firstTime).Days)
+                .Select(offset => firstTime.AddDays(offset))
+                .Where(d => dayOfWeeks.Contains(d.DayOfWeek))
+                .ToList();
+            
+            DateTime [] dateTimes = new DateTime[2];
+    
+            dateTimes[0] = dates[1];
+            dateTimes[1] = dates[2];
+
+            return dateTimes.ToList();
+        }
+    }
+}
