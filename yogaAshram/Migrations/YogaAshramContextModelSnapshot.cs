c3b938305f40d2604100663992f61f25772eda90
Iskander Dosgali <idosgali@gmail.com>
2020-08-27 13:41:00 +0600

    #114 Структура бд YogaAshramContextModelSnapshot.cs изменился

--- yogaAshram/Migrations/YogaAshramContextModelSnapshot.cs
+++ yogaAshram/Migrations/YogaAshramContextModelSnapshot.cs
@@ -307,8 +307,8 @@ namespace yogaAshram.Migrations
                     b.Property<string>("PhoneNumber")
                         .HasColumnType("text");
 
-                    b.Property<string>("Sickness")
-                        .HasColumnType("text");
+                    b.Property<long?>("SicknessId")
+                        .HasColumnType("bigint");
 
                     b.Property<string>("Source")
                         .HasColumnType("text");
@@ -326,6 +326,8 @@ namespace yogaAshram.Migrations
                     b.HasIndex("GroupId");
 
                     b.HasIndex("MembershipId");
+
+                    b.HasIndex("SicknessId");
 
                     b.ToTable("Clients");
                 });
@@ -795,6 +797,63 @@ namespace yogaAshram.Migrations
                     b.ToTable("Schedules");
                 });
 
+            modelBuilder.Entity("yogaAshram.Models.Sickness", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint")
+                        .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+                    b.Property<string>("Name")
+                        .HasColumnType("text");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Sicknesses");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 10000L,
+                            Name = "остеохондроз"
+                        },
+                        new
+                        {
+                            Id = 10001L,
+                            Name = "грыжа"
+                        },
+                        new
+                        {
+                            Id = 10002L,
+                            Name = "сколиоз"
+                        },
+                        new
+                        {
+                            Id = 10003L,
+                            Name = "артрит"
+                        },
+                        new
+                        {
+                            Id = 10004L,
+                            Name = "гипертония"
+                        },
+                        new
+                        {
+                            Id = 10005L,
+                            Name = "сахарный диабет"
+                        },
+                        new
+                        {
+                            Id = 10006L,
+                            Name = "сердечная недостаточность"
+                        },
+                        new
+                        {
+                            Id = 10007L,
+                            Name = "ожирение"
+                        });
+                });
+
             modelBuilder.Entity("yogaAshram.Models.TrialUsers", b =>
                 {
                     b.Property<long>("Id")
@@ -951,6 +1010,10 @@ namespace yogaAshram.Migrations
                     b.HasOne("yogaAshram.Models.Membership", "Membership")
                         .WithMany()
                         .HasForeignKey("MembershipId");
+
+                    b.HasOne("yogaAshram.Models.Sickness", "Sickness")
+                        .WithMany()
+                        .HasForeignKey("SicknessId");
                 });
 
             modelBuilder.Entity("yogaAshram.Models.ClientsMembership", b =>
