17a90109bf50f50dc949124f2822e7e93cb1343d
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-09-03 14:09:03 +0600

    #133 добавил методы в ClientServices.cs

--- yogaAshram/Services/ClientServices.cs
+++ yogaAshram/Services/ClientServices.cs
@@ -22,9 +22,16 @@ namespace yogaAshram.Services
             {
                 dayOfWeeks[i] = calendarEvents[i].DayOfWeek;
             }
-            DateTime fortyDays = firstTime.AddDays(40);
             
-            List<DateTime> dates = Enumerable.Range(0, 1 + fortyDays.Subtract(firstTime).Days)
+            DateTime days;
+            if(dayOfWeeks.Length == 3)
+                days = firstTime.AddDays(40);
+            else if(dayOfWeeks.Length == 2) 
+                days = firstTime.AddDays(50);
+            else
+                days = firstTime.AddDays(90);
+            
+            List<DateTime> dates = Enumerable.Range(0,  days.Subtract(firstTime).Days)
                 .Select(offset => firstTime.AddDays(offset))
                 .Where(d => dayOfWeeks.Contains(d.DayOfWeek))
                 .ToList();
@@ -36,8 +43,8 @@ namespace yogaAshram.Services
             }
             
             return dateTimes.ToList();
-        } 
-        public List<DateTime> DatesSkipFirst(DateTime firstTime, long groupId, int attendanceDays)
+        }
+        public DateTime EndDateForClientsMembership(DateTime firstTime, long groupId, int attendanceDays)
         {
             List<CalendarEvent> calendarEvents = _db.CalendarEvents.Where(c => c.GroupId == groupId).ToList();
             DayOfWeek[] dayOfWeeks = new DayOfWeek[calendarEvents.Count];
@@ -45,20 +52,51 @@ namespace yogaAshram.Services
             {
                 dayOfWeeks[i] = calendarEvents[i].DayOfWeek;
             }
-            DateTime fortyDays = firstTime.AddDays(43);
             
-            List<DateTime> dates = Enumerable.Range(0, 1 + fortyDays.Subtract(firstTime).Days)
+            DateTime days;
+            if(dayOfWeeks.Length == 3)
+                days = firstTime.AddDays(40);
+            else if(dayOfWeeks.Length == 2) 
+                days = firstTime.AddDays(50);
+            else
+                days = firstTime.AddDays(90);
+            
+            List<DateTime> dates = Enumerable.Range(0,  days.Subtract(firstTime).Days)
                 .Select(offset => firstTime.AddDays(offset))
                 .Where(d => dayOfWeeks.Contains(d.DayOfWeek))
                 .ToList();
             
-            DateTime [] dateTimes = new DateTime[attendanceDays + 1];
+            DateTime [] dateTimes = new DateTime[attendanceDays];
             for (int i = 0; i < attendanceDays; i++)
             {
                 dateTimes[i] = dates[i];
             }
-            return dateTimes.ToList();
-        } 
+            return dateTimes[^1];
+        }
+        public DateTime DateIfFrozen(DateTime lastDay, long groupId)
+        {
+            List<CalendarEvent> calendarEvents = _db.CalendarEvents.Where(c => c.GroupId == groupId).ToList();
+            DayOfWeek[] dayOfWeeks = new DayOfWeek[calendarEvents.Count];
+            for (int i = 0; i < calendarEvents.Count; i++)
+            {
+                dayOfWeeks[i] = calendarEvents[i].DayOfWeek;
+            }
+            
+            DateTime days = lastDay.AddDays(10);
+           
+            
+            List<DateTime> dates = Enumerable.Range(0,  days.Subtract(lastDay).Days)
+                .Select(offset => lastDay.AddDays(offset))
+                .Where(d => dayOfWeeks.Contains(d.DayOfWeek))
+                .ToList();
+            
+            DateTime [] dateTimes = new DateTime[2];
+            for (int i = 0; i < 2; i++)
+            {
+                dateTimes[i] = dates[i];
+            }
+            return dateTimes[^1];
+        }
         public List<DateTime> TwoTimesTrial(long? groupId, DateTime firstTime)
         {
             List<CalendarEvent> calendarEvents = _db.CalendarEvents.Where(c => c.GroupId == groupId).ToList();
