35a5dfa6f187c59ad51f6730322584d7f96633b2
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-09-07 13:08:51 +0600

    #149 добавил ClientTableViewMode.cs

---
+++ yogaAshram/Models/ModelViews/ClientTableViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace yogaAshram.Models.ModelViews
+{
+    public class ClientTableViewModel
+    {
+        public Client Client { get; set; }
+        public Attendance Attendance { get; set; }
+    }
+}
