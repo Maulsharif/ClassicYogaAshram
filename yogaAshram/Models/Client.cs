1e5eeef62e492b9ee33b4436663a833aa4c23923
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-10-04 15:04:16 +0600

    #204 добавил булевое поле в Client.cs

--- yogaAshram/Models/Client.cs
+++ yogaAshram/Models/Client.cs
@@ -44,7 +44,7 @@ namespace yogaAshram.Models
         
         public string NameSurname { get; set; }
         public DateTime DateOfBirth { get; set; }
-
+        public bool HasMembership { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
