a032798c20f8af0b06c46e2d9ae9f10d88129559
Iskander Dosgali <idosgali@gmail.com>
2020-10-16 10:56:11 +0600

    Merge branch 'master' of C:\csharp\ESDP\ClassicYogaAshram with conflicts.

---
+++ yogaAshram/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using yogaAshram.Models;
+using yogaAshram.Models.HelperModels;
+using yogaAshram.Services;
+
+namespace yogaAshram.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly YogaAshramContext _db;
+        private readonly UserManager<Employee> _userManager;
+
+        public HomeController(YogaAshramContext db, UserManager<Employee> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        // GET
+      
+        // public IActionResult Index()
+        // {
+        //     // return View();
+        // }
+    }
+}
